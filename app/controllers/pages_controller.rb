ee95a4aee08bcedf927916f4a912d43bd7f3ea0f
Chris Perkins <chrisperkins99@gmail.com>
2011-06-28 17:06:39 -0400

    title tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
   def home
     @foo = 'bar'
+    @title = 'Home'
   end
 
   def contact
+    @title = 'Contact'
   end
 
   def about
+    @title = 'About'
   end
 
 end
