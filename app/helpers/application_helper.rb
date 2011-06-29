83d44890527ef4858b7ade5597cd97eeeed77e2c
Chris Perkins <chrisperkins99@gmail.com>
2011-06-29 07:10:46 -0400

    refactoring tests, playing with blueprint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def title
+    base_title = 'Rails Tutorial App'
+    @title.nil? ? base_title : "#{base_title} | #{@title}"
+  end
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,39 +3,26 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", content: "Rails Tutorial App | Home")
-    end
+  def check_title(expected)
+    response.should have_selector("title", content: "Rails Tutorial App | #{expected}")
   end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title", content: "Rails Tutorial App | Contact")
-    end
-  end
-
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title", content: "Rails Tutorial App | About")
+  @@pages = [
+             ['home',    'Home'],
+             ['contact', 'Contact'],
+             ['about',   'About'],
+            ]
+  
+  @@pages.each do |action, title|
+    describe "GET '#{action}'" do
+      it "should be successful" do
+        get action
+        response.should be_success
+      end
+      it "should have the right title" do
+        get action
+        check_title(title)
+      end
     end
   end
 
